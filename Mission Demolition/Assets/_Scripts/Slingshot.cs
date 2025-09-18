70160b5769845161f1ed4ba47b824fa9d7024c12
Conner Homan <connermhoman@gmail.com>
2025-09-17 20:49:20 -0400

    Updated Mission Demolition
    Added more code and line tracing

--- Mission Demolition/Assets/_Scripts/Slingshot.cs
+++ Mission Demolition/Assets/_Scripts/Slingshot.cs
@@ -6,6 +6,7 @@ public class Slingshot : MonoBehaviour {
     [Header("Inscribed")]
     public GameObject projectilePrefab;
     public float velocityMult = 10f;
+    public GameObject projLinePrefab;
 
     [Header("Dynamic")]
     public GameObject launchPoint;
@@ -71,6 +72,8 @@ public class Slingshot : MonoBehaviour {
             projRB.collisionDetectionMode = CollisionDetectionMode.Continuous;
             projRB.velocity = -mouseDelta * velocityMult;
             FollowCam.POI = projectile; //Set the _MainCamera POI
+            //Add a Projectile Line to the Projectile
+            Instantiate<GameObject>(projLinePrefab, projectile.transform);
             projectile = null;
         }
     }
