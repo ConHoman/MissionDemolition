aa068accce4b734b985c2c28d85a917f87752493
Conner Homan <connermhoman@gmail.com>
2025-09-23 20:14:14 -0400

    Mission Demolition Update
    Updated the projectile line to fade after shot

--- Mission Demolition/Assets/_Scripts/ProjectileLine.cs
+++ Mission Demolition/Assets/_Scripts/ProjectileLine.cs
@@ -1,11 +1,12 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
-public class ProjectileLine : MonoBehaviour {
-    static List <ProjectileLine> PROJ_LINES = new List<ProjectileLine>();
+public class ProjectileLine: MonoBehaviour{
+    static List <ProjectileLine> PROJ_LINES = new List <ProjectileLine>();
     private const float DIM_MULT = 0.75f;
+
     private LineRenderer _line;
     private bool _drawing = true;
     private Projectile _projectile;
@@ -21,32 +22,34 @@ public class ProjectileLine : MonoBehaviour {
     }
 
     void FixedUpdate(){
-        private void OnDestroy(){
-            //Remove this Projectile Line from PROJ_LINES
-            PROJ_LINES.Remove(this);
-        }
-
-        static void ADD_LINE(ProjectileLine newLine){
-            Color col;
-            //Iterate over all the old lines and dim them
-            foreach (ProjectileLin pl in PROJ_LINES){
-                col = col * DIM_MULT;
-                pl._line.startColor = pl.line.endColor = col;
-            }
-            //Add newLine to the list
-            PROJ_LINES.Add(newLine);
-        }
-        }
         if (_drawing){
             _line.positionCount++;
-            _line.SetPosition(_line.positionCount - 1, transform.position);
+            _line.SetPosition(_line.positionCount -1, transform.position);
             //If the Projectile Rigidbody is sleeping, stop drawing
             if (_projectile != null){
-                if(!_projectile.awake){
+                if (!_projectile.awake){
                     _drawing = false;
                     _projectile = null;
                 }
             }
         }
+    }
+
+    private void OnDestroy(){
+        //Remove this ProjectileLine from PROJ_LINES
+        PROJ_LINES.Remove(this);
+    }
+
+    static void ADD_LINE(ProjectileLine newLine){
+        Color col;
+        //Iterate over all the old lines and dim them
+        foreach (ProjectileLine pl in PROJ_LINES){
+            col = pl._line.startColor;
+            col = col * DIM_MULT;
+            pl._line.startColor = pl._line.endColor = col;
+        }
+
+        //Add newLine to the list
+        PROJ_LINES.Add(newLine);
     }
 }
